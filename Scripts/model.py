7458f7f1d9997073bdcd5b6e10eb3ddfdf49dcf4
Anuj Attri <anujattri01@gmail.com>
2020-07-19 22:49:54 +0530

    Updated model.py

--- Scripts/model.py
+++ Scripts/model.py
@@ -6,7 +6,7 @@ class FacialExpressionModel(object):
 
     EMOTELIST = ["Angry", "Nauseated",
                      "Fear", "Happy",
-                     "Neutral", "Sad",
+                     "Blank", "Sad",
                      "Calm"]
 
     def __init__(self, fer_model_file, fer_weights_file):

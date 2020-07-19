7d0df382f71ee22c6741c2124d721942d54944e1
Anuj Attri <anujattri01@gmail.com>
2020-07-19 21:16:07 +0530

    Resolved Port error and corrected file paths.

---
+++ Scripts/model.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import model_from_json
+import numpy as np
+import emoji
+
+class FacialExpressionModel(object):
+
+    EMOTELIST = ["angry", "nauseated",
+                     "fear", "Happy",
+                     "neutral", "crying",
+                     "Surprised"]
+
+    def __init__(self, fer_model_file, fer_weights_file):
+        # load Model from JSON file
+        with open(fer_model_file, "r") as json_file:
+            loaded_model_json = json_file.read()
+            self.loaded_model = model_from_json(loaded_model_json)
+
+        # load weights into the new Model
+        self.loaded_model.load_weights(fer_weights_file)
+        self.loaded_model._make_predict_function()
+
+    def predict_emotion(self, img):
+        self.preds = self.loaded_model.predict(img)
+        return FacialExpressionModel.EMOTELIST[np.argmax(self.preds)]
